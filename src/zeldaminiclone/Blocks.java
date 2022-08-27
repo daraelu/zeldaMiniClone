63ef0949578bd89623009e1b17a06d78bba8e5a3
rafael <daraelu@gmail.com>
2022-08-27 17:56:00 -0300

    Iniciado Jogo 2D Zelda

---
+++ src/zeldaminiclone/Blocks.java
@@ -0,0 +1,17 @@
+package zeldaminiclone;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+import zeldaminiclone.Spritesheet;
+
+public class Blocks extends Rectangle {
+	public Blocks(int x, int y) {
+		super(x,y,32,32);
+	}
+	
+	public void Render(Graphics g){
+		
+		g.drawImage(Spritesheet.tileWall, x, y, 32, 32, null);
+	}
+}
